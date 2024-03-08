f04f61d959129a38f673d8322165d99e6ce4759a
Ahmad Omar <ahmadomar.amego@gmail.com>
2024-03-08 12:46:55 +0300

    add publish and deploy next js app to github
    Signed-off-by: Ahmad Omar <ahmadomar.amego@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+const repo = 'blockchain-website'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+}
