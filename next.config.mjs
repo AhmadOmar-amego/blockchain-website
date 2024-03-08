0f47bb8cd11cf16e6b1580d74d8f52fd58a82ac2
Ahmad Omar <ahmadomar.amego@gmail.com>
2024-03-08 16:00:49 +0300

    remove not wanted code and fix some bugs
    Signed-off-by: Ahmad Omar <ahmadomar.amego@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/blockchain-website",
+    // basePath: "/blockchain-website",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
